a7fa42a1f549e0a15b08a0b489857fbe36f081cd
darek.uchman@gmail.com <darek.uchman@gmail.com@7e42dd87-9e94-c139-7c58-0f7fc2613379>
2014-12-06 15:02:28 +0000

    fluent nhibernate

---
+++ AuditTrialServer/Helpers/NHibernateHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuditTrialServer.Entities;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace AuditTrialServer.Helpers
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+
+                    InitializeSessionFactory();
+                return _sessionFactory;
+            }
+        }
+
+        private static void InitializeSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008
+                  .ConnectionString(
+                  @"Server=(local);initial CATALOG=AuditTrialService;
+		Integrated Security=true;") // Modify your ConnectionString
+                              .ShowSql()
+                )
+                .Mappings(m =>
+                          m.FluentMappings
+                              .AddFromAssemblyOf<AuditTrialRecord>())
+                .ExposeConfiguration(cfg => new SchemaUpdate(cfg)
+                                                .Execute(false, true))
+                .BuildSessionFactory();
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
