bdf65fd40d4a24ade5677cc24a52950aec6c63c3
darek.uchman@gmail.com <darek.uchman@gmail.com@7e42dd87-9e94-c139-7c58-0f7fc2613379>
2014-12-06 15:02:28 +0000

    fluent nhibernate

---
+++ trunk/AuditTrialServer/Mappings/AuditTrialRecordMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuditTrialServer.Entities;
+using FluentNHibernate.Mapping;
+
+namespace AuditTrialServer.Mappings
+{
+    public class AuditTrialRecordMap:ClassMap<AuditTrialRecord>
+    {
+
+        public AuditTrialRecordMap() {
+
+            Id(x => x.Id);
+            Map(x => x.Description);
+        }
+
+    }
+}
---
+++ trunk/ConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuditTrialServer.Helpers;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var item = new AuditTrialServer.Entities.AuditTrialRecord() {Description = "One" };
+                    session.Save(item);
+                    transaction.Commit();
+                    Console.WriteLine("Department Created: " + item.Description);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
